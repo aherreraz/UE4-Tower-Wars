71bb0a26f4cce20265222a9f8b628eceb7b9ad94
Andres Herrera <arhz29@gmail.com>
2018-10-28 18:03:31 -0500

    Created project and added documentation * Added game design document (GDD) * Added high concept document * Added game description

---
+++ Source/TowerWars/TowerWarsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerWarsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERWARS_API ATowerWarsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
