9f88cfa6578b1382c08daad85e43736a416ba7e1
Andres Herrera <arhz29@gmail.com>
2018-11-09 12:46:59 -0500

    Implemented tower upgrade system - Added tower upgrade system - Added some materials and textures from StandartContent for prototyping

--- Source/TowerWars/Private/Tower.cpp
+++ Source/TowerWars/Private/Tower.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tower.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "SelectionInterface.h"
 
 bool ATower::OnSelectionGained_Implementation()
 {
@@ -15,4 +18,23 @@ bool ATower::OnSelectionLost_Implementation()
 ESelectedType ATower::GetType_Implementation() const
 {
 	return ESelectedType::Tower;
+}
+
+void ATower::ReplaceTower(TSubclassOf<ATower> NewTowerClass)
+{
+	ATower* NewTower = nullptr;
+	return ReplaceTower(NewTowerClass, &NewTower);
+}
+
+void ATower::ReplaceTower(TSubclassOf<ATower> NewTowerClass, ATower ** OutNewTower)
+{
+	// TODO: Check cost
+	ATower* const NewTower = GetWorld()->SpawnActorDeferred<ATower>(NewTowerClass, GetTransform(), nullptr, nullptr, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+	if (NewTower)
+	{
+		UGameplayStatics::FinishSpawningActor(NewTower, GetTransform());
+		ISelectionInterface::Execute_OnSelectionLost(this);
+		SetLifeSpan(0.1f);
+	}
+	*OutNewTower = NewTower;
 }
--- Source/TowerWars/Public/Tower.h
+++ Source/TowerWars/Public/Tower.h
@@ -12,10 +12,23 @@ class TOWERWARS_API ATower : public AActor, public ISelectionInterface
 {
 	GENERATED_BODY()
 
+public:
+
 	/* ISelectionInterface Implementation */
 	virtual bool OnSelectionGained_Implementation() override;
 
 	virtual bool OnSelectionLost_Implementation() override;
 
 	virtual ESelectedType GetType_Implementation() const override;
+
+	/* Construction */
+	UFUNCTION(BlueprintCallable, Category = Tower)
+	void ReplaceTower(TSubclassOf<ATower> NewTowerClass);
+
+	void ReplaceTower(TSubclassOf<ATower> NewTowerClass, ATower** OutNewTower);
+
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = Tower)
+	int32 GoldCost;
 };
